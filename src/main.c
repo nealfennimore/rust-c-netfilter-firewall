65683b916a037022f21169c359354c3625068046
Neal Fennimore <hi@neal.codes>
2021-11-13 11:00:20 -0500

    Update to use MSG_DONTWAIT

--- src/main.c
+++ src/main.c
@@ -7,58 +7,16 @@
 #include <libnetfilter_queue/libnetfilter_queue.h>
 
 /* returns packet id */
-static u_int32_t print_pkt(struct nfq_data *tb)
+static u_int32_t get_id(struct nfq_data *tb)
 {
     int id = 0;
     struct nfqnl_msg_packet_hdr *ph;
-    struct nfqnl_msg_packet_hw *hwph;
-    u_int32_t mark, ifi;
-    int ret;
-    char *data;
 
     ph = nfq_get_msg_packet_hdr(tb);
     if (ph)
     {
         id = ntohl(ph->packet_id);
-        printf("hw_protocol=0x%04x hook=%u id=%u ",
-               ntohs(ph->hw_protocol), ph->hook, id);
     }
-
-    hwph = nfq_get_packet_hw(tb);
-    if (hwph)
-    {
-        int i, hlen = ntohs(hwph->hw_addrlen);
-
-        printf("hw_src_addr=");
-        for (i = 0; i < hlen - 1; i++)
-            printf("%02x:", hwph->hw_addr[i]);
-        printf("%02x ", hwph->hw_addr[hlen - 1]);
-    }
-
-    mark = nfq_get_nfmark(tb);
-    if (mark)
-        printf("mark=%u ", mark);
-
-    ifi = nfq_get_indev(tb);
-    if (ifi)
-        printf("indev=%u ", ifi);
-
-    ifi = nfq_get_outdev(tb);
-    if (ifi)
-        printf("outdev=%u ", ifi);
-    ifi = nfq_get_physindev(tb);
-    if (ifi)
-        printf("physindev=%u ", ifi);
-
-    ifi = nfq_get_physoutdev(tb);
-    if (ifi)
-        printf("physoutdev=%u ", ifi);
-
-    ret = nfq_get_payload(tb, &data);
-    if (ret >= 0)
-        printf("payload_len=%d ", ret);
-
-    fputc('\n', stdout);
 
     return id;
 }
@@ -66,9 +24,8 @@ static u_int32_t print_pkt(struct nfq_data *tb)
 static int cb(struct nfq_q_handle *qh, struct nfgenmsg *nfmsg,
               struct nfq_data *nfa, void *data)
 {
-    u_int32_t id = print_pkt(nfa);
-    printf("entering callback\n");
-    return nfq_set_verdict(qh, id, NF_ACCEPT, 0, NULL);
+    u_int32_t id = get_id(nfa);
+    return nfq_set_verdict2(qh, id, NF_ACCEPT, 0, 0, NULL);
 }
 
 int main(int argc, char **argv)
@@ -77,7 +34,8 @@ int main(int argc, char **argv)
     struct nfq_q_handle *qh;
     int fd;
     int rv;
-    char buf[4096] __attribute__((aligned));
+    int queue_num = 0;
+    char *buf = calloc(UINT32_MAX, sizeof(char));
 
     printf("opening library handle\n");
     h = nfq_open();
@@ -102,7 +60,7 @@ int main(int argc, char **argv)
     }
 
     printf("binding this socket to queue '0'\n");
-    qh = nfq_create_queue(h, 1, &cb, NULL);
+    qh = nfq_create_queue(h, queue_num, &cb, NULL);
     if (!qh)
     {
         fprintf(stderr, "error during nfq_create_queue()\n");
@@ -118,9 +76,11 @@ int main(int argc, char **argv)
 
     fd = nfq_fd(h);
 
-    while ((rv = recv(fd, buf, sizeof(buf), 0)) && rv >= 0)
+    while ((rv = recv(fd, buf, UINT32_MAX, MSG_DONTWAIT)))
     {
-        printf("pkt received\n");
+        if(rv < 0){
+            continue;
+        }
         nfq_handle_packet(h, buf, rv);
     }
 
@@ -136,6 +96,7 @@ int main(int argc, char **argv)
 
     printf("closing library handle\n");
     nfq_close(h);
+    free(buf);
 
     exit(0);
 }
